6ebab168011edc0238d90d499e8976c28d1ad159
中島浩二 <knaka0209@gmail.com>
2021-02-19 14:44:07 +0900

    up,0219a2

--- next.config.js
+++ next.config.js
@@ -4,6 +4,23 @@ module.exports = {
     CSRF_SECRET : 'secret1234',
     BASE_URL: "http://localhost:3001",
     MONGODB_URL: "mongodb://localhost:27017",
-    MONGODB_DB_NAME: "hcms",    
+    MONGODB_DB_NAME: "hcms",
+  },
+  async headers() {
+    return [
+      {
+        source: '/:api*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Origin, X-Requested-With, Content-Type, Accept',
+          },
+        ],
+      },
+    ]
   },
 }
